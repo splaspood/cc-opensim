aca48fd6624d90d99e302091f3894138fc960de6
James W. Brinkerhoff <jwb@paravolve.net>
2012-02-27 00:38:56 -0500

    Correct database type attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
-default['opensim']['install_prefix']  = "/usr/local/opensim"
-default['opensim']['version']         = "0.7.2"
-default['opensim']['checksum']        = "724787d3c2489fcb6e789e4168d9569fd0f30206"
-default['opensim']['url']             = "http://opensimulator.org/dist/opensim-#{node['opensim']['version']}-bin.tar.gz"
+default['opensim']['install_prefix']   = "/usr/local/opensim"
+default['opensim']['version']          = "0.7.2"
+default['opensim']['checksum']         = "724787d3c2489fcb6e789e4168d9569fd0f30206"
+default['opensim']['url']              = "http://opensimulator.org/dist/opensim-#{node['opensim']['version']}-bin.tar.gz"
 
-default['opensim']['mode']            = "standalone"
-default['opensim']['database']        = "mysql"
+default['opensim']['mode']             = "standalone"
+default['opensim']['database']['type'] = "mysql"
 
 default['opensim']['admin']['first_name'] = 'Admin'
 default['opensim']['admin']['last_name'] = 'User'
